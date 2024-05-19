81c36a037213e517ecec8a68e1d5c257fc27b9da
Laurentia Kayleen Christopher <18221053@std.stei.itb.ac.id>
2024-05-19 23:12:42 +0700

    Classes major update + wasong new logo

---
+++ src/app/classes/Classes copy.tsx
@@ -0,0 +1,113 @@
+import React from "react";
+import "../../styles/classes.css";
+
+export const Classes = (): JSX.Element => {
+  return (
+    <div className="classes">
+      <div className="div">
+        <div className="overlap">
+          <div className="group">
+            <div className="text-wrapper">Home</div>
+            <div className="text-wrapper-2">Classes</div>
+            <div className="text-wrapper-3">Assignments</div>
+          </div>
+          <div className="overlap-group-wrapper">
+            <div className="overlap-group">
+              <div className="wa-ong">Wa&nbsp;&nbsp;&nbsp;&nbsp;ong</div>
+              <img className="screenshot" alt="Screenshot" src="/images/screenshot-2024-05-03-at-18-30-1.png" />
+            </div>
+          </div>
+          <div className="text-wrapper-4">My Profile</div>
+        </div>
+        <div className="group-2">
+          <div className="group-3">
+            <div className="text-wrapper-5">Acoustic Guitar</div>
+            <div className="text-wrapper-6">Rp 1.500.000/month</div>
+          </div>
+          <img className="best-acoustic" alt="Best acoustic" src="/images/best-acoustic-guitars-1.png" />
+        </div>
+        <div className="overlap-wrapper">
+          <div className="overlap-2">
+            <div className="div-wrapper">
+              <div className="text-wrapper-7">Electric Keyboard</div>
+            </div>
+            <img className="img" alt="Screenshot" src="/images/screenshot-2024-05-03-at-19-16-1.png" />
+            <div className="overlap-group-2">
+              <div className="text-wrapper-5">Rp 1.200.000/month</div>
+              <div className="text-wrapper-5">Rp 1.200.000/month</div>
+            </div>
+          </div>
+        </div>
+        <div className="group-4">
+          <div className="overlap-2">
+            <div className="group-5">
+              <div className="text-wrapper-8">Violin</div>
+            </div>
+            <img className="img-2" alt="Screenshot" src="/images/screenshot-2024-05-03-at-19-17-1.png" />
+            <p className="p">
+              Next Class: Saturday, 4 May 2024
+              <br />
+              Time: 13:00 - 14.30
+            </p>
+          </div>
+        </div>
+        <div className="group-6">
+          <div className="overlap-2">
+            <div className="group-7">
+              <div className="text-wrapper-9">Cello</div>
+            </div>
+            <img className="img-2" alt="Screenshot" src="/images/screenshot-2024-05-03-at-19-19-1.png" />
+            <div className="text-wrapper-10">Rp 1.450.000/month</div>
+          </div>
+        </div>
+        <div className="group-8">
+          <div className="overlap-2">
+            <div className="group-9">
+              <div className="text-wrapper-11">Classical Piano</div>
+            </div>
+            <img className="img" alt="Maxresdefault" src="/images/maxresdefault-1.png" />
+            <p className="p">
+              Next Class: Tuesday, 2 May 2024
+              <br />
+              Time: 13:00 - 14.30
+            </p>
+          </div>
+        </div>
+        <div className="group-10">
+          <div className="overlap-2">
+            <div className="group-11">
+              <div className="text-wrapper-12">Trombone</div>
+            </div>
+            <img className="img-2" alt="Trombone quickstart" src="/images/trombone-quickstart-guide-1.png" />
+            <div className="text-wrapper-10">Rp 1.000.000/month</div>
+          </div>
+        </div>
+        <div className="group-12">
+          <div className="overlap-2">
+            <div className="group-13">
+              <div className="text-wrapper-13">Saxophone</div>
+            </div>
+            <img className="img-2" alt="Saxophone buying" src="/images/saxophone-buying-guide-featured-image-1.png" />
+            <div className="text-wrapper-10">Rp 1.250.000/month</div>
+          </div>
+        </div>
+        <div className="group-14">
+          <div className="overlap-2">
+            <div className="group-15">
+              <div className="text-wrapper-14">Flute</div>
+            </div>
+            <img className="img-2" alt="Dsc" src="/images/dsc-3600-1.png" />
+            <div className="text-wrapper-10">Rp 1.450.000/month</div>
+          </div>
+        </div>
+        <div className="text-wrapper-15">Classes</div>
+        <div className="text-wrapper-16">Enrolled Classes</div>
+        <div className="overlap-3">
+          <p className="text-wrapper-17">Your Music Journey Starts Here!</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Classes;
+
--- src/app/classes/Classes.tsx
+++ src/app/classes/Classes.tsx
@@ -1,112 +1,153 @@
 import React from "react";
 import "../../styles/classes.css";
 
-export const Classes = (): JSX.Element => {
+const Classes = () => {
   return (
-    <div className="classes">
-      <div className="div">
-        <div className="overlap">
-          <div className="group">
-            <div className="text-wrapper">Home</div>
-            <div className="text-wrapper-2">Classes</div>
-            <div className="text-wrapper-3">Assignments</div>
+    <div className="flex flex-col bg-white">
+      <div className="flex gap-5 justify-between py-3.5 pr-14 pl-6 w-full text-lg bg-orange-50 text-stone-900 max-md:flex-wrap max-md:px-5 max-md:max-w-full">
+        <img
+          loading="lazy"
+          srcSet="/images/wasong-logo.png"
+          className="shrink-0 max-w-full w-[126px]"
+        />
+        <div className="flex gap-5 my-auto max-md:flex-wrap max-md:max-w-full">
+          <div className="grow">Home</div>
+          <div className="font-bold">Classes</div>
+          <div className="flex-auto">Assignments</div>
+          <div className="text-center capitalize text-neutral-950">
+            My Profile
           </div>
-          <div className="overlap-group-wrapper">
-            <div className="overlap-group">
-              <div className="wa-ong">Wa&nbsp;&nbsp;&nbsp;&nbsp;ong</div>
-              <img className="screenshot" alt="Screenshot" src="/images/screenshot-2024-05-03-at-18-30-1.png" />
+        </div>
+      </div>
+      <div className="flex flex-col px-11 mt-14 w-full max-md:px-5 max-md:mt-10 max-md:max-w-full">
+        <div className="text-5xl font-bold text-zinc-800 max-md:max-w-full max-md:text-4xl">
+          Classes
+        </div>
+        <div className="mt-14 max-md:mt-10 max-md:max-w-full">
+          <div className="flex gap-5 max-md:flex-col max-md:gap-0">
+            <div className="flex flex-col ml-5 w-3/12 max-md:ml-0 max-md:w-full">
+              <div className="flex flex-col grow px-3 py-6 w-full text-xl font-medium text-black bg-white rounded-2xl shadow-lg max-md:mt-9">
+                <img
+                  loading="lazy"
+                  srcSet="/images/best-acoustic-guitars-1.png"
+                  className="w-full aspect-[1.56]"
+                />
+                <div className="mt-5">Acoustic Guitar</div>
+                <div className="mt-10 max-md:mt-10">Rp 1.500.000/month </div>
+              </div>
+            </div>
+            <div className="flex flex-col ml-5 w-3/12 max-md:ml-0 max-md:w-full">
+              <div className="flex flex-col grow px-3 py-6 w-full text-xl font-medium text-black bg-white rounded-2xl shadow-lg max-md:mt-9">
+                <img
+                  loading="lazy"
+                  srcSet="/images/screenshot-2024-05-03-at-19-16-1.png"
+                  className="w-full aspect-[1.59]"
+                />
+                <div className="mt-5">Electric Keyboard</div>
+                <div className="mt-10 max-md:mt-10">Rp 1.200.000/month </div>
+              </div>
+            </div>
+            <div className="flex flex-col ml-5 w-3/12 max-md:ml-0 max-md:w-full">
+              <div className="flex flex-col grow px-3 py-6 w-full text-xl font-medium text-black bg-white rounded-2xl shadow-lg max-md:mt-9">
+                <img
+                  loading="lazy"
+                  srcSet="/images/trombone-quickstart-guide-1.png"
+                  className="w-full aspect-[1.59]"
+                />
+                <div className="mt-5">Trombone</div>
+                <div className="mt-10 max-md:mt-10">Rp 1.000.000/month </div>
+              </div>
+            </div>
+            <div className="flex flex-col ml-5 w-3/12 max-md:ml-0 max-md:w-full">
+              <div className="flex flex-col grow px-3 py-6 w-full text-xl font-medium text-black bg-white rounded-2xl shadow-lg max-md:mt-9">
+                <img
+                  loading="lazy"
+                  srcSet="/images/screenshot-2024-05-03-at-19-19-1.png"
+                  className="w-full aspect-[1.59]"
+                />
+                <div className="mt-5">Cello</div>
+                <div className="mt-10 max-md:mt-10">Rp 1.450.000/month </div>
+              </div>
             </div>
           </div>
-          <div className="text-wrapper-4">My Profile</div>
         </div>
-        <div className="group-2">
-          <div className="group-3">
-            <div className="text-wrapper-5">Acoustic Guitar</div>
-            <div className="text-wrapper-6">Rp 1.500.000/month</div>
-          </div>
-          <img className="best-acoustic" alt="Best acoustic" src="/images/best-acoustic-guitars-1.png" />
-        </div>
-        <div className="overlap-wrapper">
-          <div className="overlap-2">
-            <div className="div-wrapper">
-              <div className="text-wrapper-7">Electric Keyboard</div>
+        <div className="mt-14 max-md:mt-10 max-md:max-w-full">
+          <div className="flex gap-5 max-md:flex-col max-md:gap-0">
+            <div className="flex flex-col ml-5 w-3/12 max-md:ml-0 max-md:w-full">
+              <div className="flex flex-col grow px-3 py-6 w-full text-xl font-medium text-black bg-white rounded-2xl shadow-lg max-md:mt-9">
+                <img
+                  loading="lazy"
+                  srcSet="/images/saxophone-buying-guide-featured-image-1.png"
+                  className="w-full aspect-[1.56]"
+                />
+                <div className="mt-5">Saxophone</div>
+                <div className="mt-10 max-md:mt-10">Rp 1.250.000/month </div>
+              </div>
             </div>
-            <img className="img" alt="Screenshot" src="/images/screenshot-2024-05-03-at-19-16-1.png" />
-            <div className="overlap-group-2">
-              <div className="text-wrapper-5">Rp 1.200.000/month</div>
-              <div className="text-wrapper-5">Rp 1.200.000/month</div>
+            <div className="flex flex-col ml-5 w-3/12 max-md:ml-0 max-md:w-full">
+              <div className="flex flex-col grow px-3 py-6 w-full text-xl font-medium text-black bg-white rounded-2xl shadow-lg max-md:mt-9">
+                <img
+                  loading="lazy"
+                  srcSet="/images/dsc-3600-1.png"
+                  className="w-full aspect-[1.56]"
+                />
+                <div className="mt-6">Flute</div>
+                <div className="mt-11 max-md:mt-10">Rp 1.450.000/month </div>
+              </div>
             </div>
-          </div>
-        </div>
-        <div className="group-4">
-          <div className="overlap-2">
-            <div className="group-5">
-              <div className="text-wrapper-8">Violin</div>
+            <div className="flex flex-col ml-5 w-3/12 max-md:ml-0 max-md:w-full">
             </div>
-            <img className="img-2" alt="Screenshot" src="/images/screenshot-2024-05-03-at-19-17-1.png" />
-            <p className="p">
-              Next Class: Saturday, 4 May 2024
-              <br />
-              Time: 13:00 - 14.30
-            </p>
+            <div className="flex flex-col ml-5 w-3/12 max-md:ml-0 max-md:w-full">
+            </div>
           </div>
         </div>
-        <div className="group-6">
-          <div className="overlap-2">
-            <div className="group-7">
-              <div className="text-wrapper-9">Cello</div>
+        <div className="mt-24 text-5xl font-bold text-zinc-800 max-md:mt-10 max-md:max-w-full max-md:text-4xl">
+          Enrolled Classes
+        </div>
+        <div className="mt-14 max-md:mt-10 max-md:max-w-full">
+          <div className="flex gap-5 max-md:flex-col max-md:gap-0">
+            <div className="flex flex-col ml-5 w-3/12 max-md:ml-0 max-md:w-full">
+              <div className="flex flex-col grow px-3 py-5 w-full text-xl font-medium text-black bg-white rounded-2xl shadow-lg max-md:mt-9">
+                <img
+                  loading="lazy"
+                  srcSet="/images/maxresdefault-1.png"
+                  className="w-full aspect-[1.59]"
+                />
+                <div className="mt-6">Classical Piano</div>
+                <div className="mt-8 mr-6 max-md:mr-2.5">
+                  Next Class: Tuesday, 2 May 2024
+                  <br />
+                  Time: 13:00 - 14.30
+                </div>
+              </div>
             </div>
-            <img className="img-2" alt="Screenshot" src="/images/screenshot-2024-05-03-at-19-19-1.png" />
-            <div className="text-wrapper-10">Rp 1.450.000/month</div>
+            <div className="flex flex-col ml-5 w-3/12 max-md:ml-0 max-md:w-full">
+              <div className="flex flex-col grow px-3 py-5 w-full text-xl font-medium text-black bg-white rounded-2xl shadow-lg max-md:mt-9">
+                <img
+                  loading="lazy"
+                  srcSet="/images/screenshot-2024-05-03-at-19-17-1.png"
+                  className="w-full aspect-[1.59]"
+                />
+                <div className="mt-5">Violin</div>
+                <div className="mt-9 max-md:mr-2.5">
+                  Next Class: Saturday, 4 May 2024
+                  <br />
+                  Time: 13:00 - 14.30
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col ml-5 w-3/12 max-md:ml-0 max-md:w-full">
+            </div>
+            <div className="flex flex-col ml-5 w-3/12 max-md:ml-0 max-md:w-full">
+            </div>
           </div>
         </div>
-        <div className="group-8">
-          <div className="overlap-2">
-            <div className="group-9">
-              <div className="text-wrapper-11">Classical Piano</div>
-            </div>
-            <img className="img" alt="Maxresdefault" src="/images/maxresdefault-1.png" />
-            <p className="p">
-              Next Class: Tuesday, 2 May 2024
-              <br />
-              Time: 13:00 - 14.30
-            </p>
-          </div>
-        </div>
-        <div className="group-10">
-          <div className="overlap-2">
-            <div className="group-11">
-              <div className="text-wrapper-12">Trombone</div>
-            </div>
-            <img className="img-2" alt="Trombone quickstart" src="/images/trombone-quickstart-guide-1.png" />
-            <div className="text-wrapper-10">Rp 1.000.000/month</div>
-          </div>
-        </div>
-        <div className="group-12">
-          <div className="overlap-2">
-            <div className="group-13">
-              <div className="text-wrapper-13">Saxophone</div>
-            </div>
-            <img className="img-2" alt="Saxophone buying" src="/images/saxophone-buying-guide-featured-image-1.png" />
-            <div className="text-wrapper-10">Rp 1.250.000/month</div>
-          </div>
-        </div>
-        <div className="group-14">
-          <div className="overlap-2">
-            <div className="group-15">
-              <div className="text-wrapper-14">Flute</div>
-            </div>
-            <img className="img-2" alt="Dsc" src="/images/dsc-3600-1.png" />
-            <div className="text-wrapper-10">Rp 1.450.000/month</div>
-          </div>
-        </div>
-        <div className="text-wrapper-15">Classes</div>
-        <div className="text-wrapper-16">Enrolled Classes</div>
-        <div className="overlap-3">
-          <p className="text-wrapper-17">Your Music Journey Starts Here!</p>
-        </div>
+      </div>
+      <div className="justify-center items-center px-16 py-14 mt-16 w-full text-7xl italic font-bold bg-orange-50 text-zinc-800 max-md:px-5 max-md:mt-10 max-md:max-w-full max-md:text-4xl">
+        Your Music Journey Starts Here!
       </div>
     </div>
   );
-};
+}
+
 export default Classes;
