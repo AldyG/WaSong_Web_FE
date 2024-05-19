51a4c93579a4b2ff31b8881939d882224dad00d1
Laurentia Kayleen Christopher <18221053@std.stei.itb.ac.id>
2024-05-19 11:21:15 +0700

    Added Class

---
+++ src/app/classes/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Classes from './Classes';
+
+function Page() {
+    return (
+        <div>
+            <Classes />
+        </div>
+    );
+}
+
+export default Page;
